74985fab861cde990de8a790297d4609614880f2
quangnt03 <55090045+quangnt03@users.noreply.github.com>
2026-09-02 16:11:21 +0700

    Add: longest-repeating-substring-with-replacement - submission-7

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-7.py
@@ -0,0 +1,19 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        # what is the longest substring with only s[i] char
+        # if len of current substring (0:i) == i - k + 1, then s[0]:s[i] is the target string
+        l = 0
+        ans = 1
+        n = len(s)
+        count = {}
+        maxfreq = 1
+        
+        for r in range(n):
+            count[s[r]] = count.get(s[r], 0) + 1
+            maxfreq = max(maxfreq, count[s[r]])
+            while r - l + 1 > maxfreq + k:
+                count[s[l]] -= 1
+                l += 1
+            ans = max(ans, r - l + 1)
+                
+        return ans
